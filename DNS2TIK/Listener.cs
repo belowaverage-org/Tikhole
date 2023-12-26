2f43dc50ab4360d24ad990b16db443deccde2469
Dylan Bickerstaff <dylan@belowaverage.org>
2023-12-26 00:01:37 -0500

    Add project files.

---
+++ DNS2TIK/Listener.cs
@@ -0,0 +1,24 @@
+﻿using System.Net;
+using System.Net.Sockets;
+
+namespace DNS2TIK
+{
+    public class Listener
+    {
+        public UdpClient Client = new();
+        public event EventHandler RecievedData;
+        public IPEndPoint IPEndPoint = new(IPAddress.Any, 53);
+        public Listener()
+        {
+            Client.Connect(IPEndPoint);
+            _ = Task.Run(() =>
+            {
+                while (Client != null)
+                {
+                    Client.Receive(ref IPEndPoint);
+                    RecievedData?.Invoke(null, EventArgs.Empty);
+                }
+            });
+        }
+    }
+}
---
+++ DNS2TIK/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace DNS2TIK
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Listener Listener = new();
+            Listener.RecievedData += Listener_RecievedData;
+            Console.ReadLine();
+        }
+
+        private static void Listener_RecievedData(object? sender, EventArgs e)
+        {
+            Console.WriteLine("Got data");
+        }
+    }
+}
